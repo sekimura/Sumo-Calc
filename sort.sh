23991e6c6859049bdae81f88ba31130cce834e95
Masayoshi Sekimura <sekimura@sixapart.com>
2011-02-08 10:17:21 -0800

    use iconv instead of lv

--- sort.sh
+++ sort.sh
@@ -1,2 +1,2 @@
 #!/bin/sh -x
-grep common12-18-333 html/*.html |lv -Iej | grep font | sed -e 's/.*common12-18-333">//g' | sed -e 's/<\/font>.*//g' | sort -n | sed -e 's/勝.*//g' | sort -n | uniq -c
+grep common12-18-333 html/*.html |iconv -f euc-jp | grep font | sed -e 's/.*common12-18-333">//g' | sed -e 's/<\/font>.*//g' | sort -n | sed -e 's/勝.*//g' | sort -n | uniq -c
